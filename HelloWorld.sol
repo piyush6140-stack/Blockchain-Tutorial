594051b2030254995248c1e7aeb1a83096f499da
piyush6140-stack <73892871+piyush6140-stack@users.noreply.github.com>
2022-07-28 12:52:02 +0530

    Create HelloWorld.sol
    First Contract

---
+++ HelloWorld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract HelloWorld{
+    
+    function print() public pure returns(string memory){
+        return "Hello World";
+    }
+
+}
